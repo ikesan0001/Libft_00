271c590ffcf6b195cea7cff51fa357eacd1e2f8f
Ikeda Ryoga <iryoga@c1r20s10.42tokyo.jp>
2022-05-05 23:01:23 +0900

    make libft.h. isascii and strlen done.

---
+++ libft.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iryoga </var/mail/iryoga>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/05 20:56:21 by iryoga            #+#    #+#             */
+/*   Updated: 2022/05/05 22:05:33 by iryoga           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef	LIBFT_H
+# define	LIBFT_H
+
+# include <stdlib.h>
+
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
+size_t	ft_strlen(const char *str);
+
+#endif
