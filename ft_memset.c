2a8dd1e06267f9c162026bb6e4b6f0143de29946
Ikeda Ryoga <iryoga@c1r20s10.42tokyo.jp>
2022-05-06 09:07:45 +0900

    memset done

--- ft_memset.c
+++ ft_memset.c
@@ -6,22 +6,20 @@
 /*   By: iryoga </var/mail/iryoga>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/05 23:06:25 by iryoga            #+#    #+#             */
-/*   Updated: 2022/05/06 00:14:43 by iryoga           ###   ########.fr       */
+/*   Updated: 2022/05/06 09:07:08 by iryoga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*(void *buf, int ch, size_t n)
+void	*ft_memset(void *buf, int ch, size_t n)
 {
-	size_t	i;
+	size_t			i;
 	unsigned char	*b;
-	unsigned char	c;
 
 	i = 0;
 	b = (unsigned char *)buf;
-	c = (unsigned char)ch;
 	while (i++ < n)
-		b[i] = c;
+		b[i] = (unsigned char *)ch;
 	return (b);
 }
