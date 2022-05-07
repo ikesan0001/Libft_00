ffb94211957a869b2d8f4295e5bdf0ef6cd5ab63
Ikeda Ryoga <iryoga@c1r20s10.42tokyo.jp>
2022-05-08 00:27:33 +0900

    memmove done.

---
+++ ft_memmove.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iryoga </var/mail/iryoga>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/07 23:05:55 by iryoga            #+#    #+#             */
+/*   Updated: 2022/05/08 00:25:25 by iryoga           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	unsigned char	*d;
+	unsigned char	*s;
+
+	d = (unsigned char *)dest;
+	s = (unsigned char *)src;
+	if (dest == src || n == 0)
+		return (dest);
+	if (dest > src)
+	{
+		while (n-- > 0)
+			d[n] = s[n];
+	}
+	else
+		dest = ft_memcpy(dest, src, n);
+	return (dest);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: iryoga </var/mail/iryoga>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/05 20:56:21 by iryoga            #+#    #+#             */
-/*   Updated: 2022/05/06 23:29:44 by iryoga           ###   ########.fr       */
+/*   Updated: 2022/05/07 10:52:24 by iryoga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,19 @@ size_t	ft_strlen(const char *str);
 void	*ft_memset(void *buf, int ch, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+//size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+//size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+//int	ft_toupper(int c);
+//int	ft_tolower(int c);
+//char	*ft_strchr(const char *s, int c);
+//char	*ft_strrchr(const char *s, int c);
+//int	ft_strncmp(const char *s1, const char *s2, size_t n);
+//void	*ft_memchr(const void *s, int c, size_t n);
+//int	ft+memcmp(const void *s1, const void *s2, size_t n);
+//char	*ft_strnstr(const char *haystak, const char *needle, size_t len);
+//int	ft_atoi(const char *str);
+//void	*ft_calloc(size_t count, size_t size);
+//char	*ft_strdup(const char *s1);
 
 #endif
