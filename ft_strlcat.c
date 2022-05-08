69952e8f3922fa082182a43b288f553232c5ec25
Ikeda Ryoga <iryoga@c1r20s10.42tokyo.jp>
2022-05-09 01:08:24 +0900

    strchr, strrchr, strncmp, strlcpy done. strlcat is difficult.

---
+++ ft_strlcat.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iryoga </var/mail/iryoga>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/08 21:45:42 by iryoga            #+#    #+#             */
+/*   Updated: 2022/05/08 23:11:45 by iryoga           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	char	*s;
+	size_t	i;
+	size_t	j;
+	
+	s = (char *)src;
+	i = 0;
+	j = ft_strlen(dst);
+	if (dstsize == 0)
+		return (ft_strlen(s));
+	while (dstsize > 0 && i < dstsize - j  - 1 && s[i] != 0)
+	{
+		dst[j + i] = s[i];
+		i++;
+	}
+	dst[j + i] = 0;
+	return (j + ft_strlen(s));
+}
---
+++ ft_strlcpy.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iryoga </var/mail/iryoga>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/08 19:01:45 by iryoga            #+#    #+#             */
+/*   Updated: 2022/05/08 22:26:36 by iryoga           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	char	*s;
+	size_t	i;
+
+	s = (char *)src;
+	i = 0;
+	while (dstsize > 0 && i < dstsize - 1 && s[i] != 0)
+	{
+		dst[i] = s[i];
+		i++;
+	}
+	if (dstsize > 0)
+		dst[i] = 0;
+	return (ft_strlen(s));
+}
