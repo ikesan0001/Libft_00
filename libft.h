c8a560ae9c1268f557dae2ae8803effcff1a8536
Ikeda Ryoga <iryoga@c1r20s10.42tokyo.jp>
2022-05-17 01:06:21 +0900

    First commit. All OK.

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: iryoga </var/mail/iryoga>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/05 20:56:21 by iryoga            #+#    #+#             */
-/*   Updated: 2022/05/09 16:55:28 by iryoga           ###   ########.fr       */
+/*   Updated: 2022/05/17 01:03:01 by iryoga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,16 +27,5 @@ void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
-int	ft_toupper(int c);
-int	ft_tolower(int c);
-char	*ft_strchr(const char *s, int c);
-char	*ft_strrchr(const char *s, int c);
-int	ft_strncmp(const char *s1, const char *s2, size_t n);
-void	*ft_memchr(const void *s, int c, size_t n);
-int	ft_memcmp(const void *s1, const void *s2, size_t n);
-char	*ft_strnstr(const char *haystak, const char *needle, size_t len);
-int	ft_atoi(const char *str);
-void	*ft_calloc(size_t count, size_t size);
-char	*ft_strdup(const char *s1);
 
 #endif
