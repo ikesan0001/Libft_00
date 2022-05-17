28775d1fa3a57a952710319028c201b340f7b906
Ikeda Ryoga <iryoga@c1r20s10.42tokyo.jp>
2022-05-17 21:24:49 +0900

    header OK.

--- libft.h
+++ libft.h
@@ -6,20 +6,20 @@
 /*   By: iryoga </var/mail/iryoga>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/05 20:56:21 by iryoga            #+#    #+#             */
-/*   Updated: 2022/05/17 01:03:01 by iryoga           ###   ########.fr       */
+/*   Updated: 2022/05/17 21:24:17 by iryoga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef	LIBFT_H
-# define	LIBFT_H
+#ifndef LIBFT_H
+# define LIBFT_H
 
 # include <stdlib.h>
 
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
 size_t	ft_strlen(const char *str);
 void	*ft_memset(void *buf, int ch, size_t n);
 void	ft_bzero(void *s, size_t n);
